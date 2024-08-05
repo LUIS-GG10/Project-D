008ea66bc7597582a7b49c9963693a43193d12aa
LUIS-GG10 <luis_enggg@hotmail.com>
2024-08-05 13:44:52 -0600

    Login Complete

--- FrontEnd/Project_1/src/app/login/componentes/formulario/formulario.component.ts
+++ FrontEnd/Project_1/src/app/login/componentes/formulario/formulario.component.ts
@@ -29,11 +29,11 @@ if (this.PRID.valid && this.Password.valid) {
   this.sendData();
 }
 }
-get_isSuccess(){
+get_isSuccess(response: any){
   const fakeToken = '12345';  // Esto es solo un ejemplo
     if (this.authService.login(this.isSuccess)) {
       this.authService.setToken(fakeToken);  // Guarda el token (esto es solo un ejemplo)
-      this.router.navigate(['/Principal']);  // Redirige a la página protegida
+      this.router.navigate(['/Principal'],{ state: { user: response }});  // Redirige a la página protegida
     } else {
       alert('Invalid credentials');
     }
@@ -46,7 +46,7 @@ get_isSuccess(){
   errorMessage = signal('');
   
   constructor(private router: Router, private http: HttpClient, private authService: AuthService) {
-    this.isSuccess = false;
+    
   }
 
   updateErrorMessage() {
@@ -85,16 +85,17 @@ get_isSuccess(){
     // Make the GET request
     this.http.post(url,data).subscribe(response => {
       console.log('Success:', response);
+      
       this.responseValidate(response);
     }, error => {
       console.error('Error:', error);
     });
   }
   responseValidate(responsedata: any){
-    if(responsedata.Message){
+    if(responsedata){
       this.isSuccess = true;
       console.log(this.isSuccess);
-      this.get_isSuccess();
+      this.get_isSuccess(responsedata);
     }
     else{
     
