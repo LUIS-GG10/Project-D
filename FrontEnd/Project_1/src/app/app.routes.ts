10d2f049dc80d86e7dd091624f1536a154bc0b04
AndreaSepulveda01 <andreasepulveda876@gmail.com>
2024-08-08 16:24:06 -0600

    Design-ForgotPassword

--- FrontEnd/Project_1/src/app/app.routes.ts
+++ FrontEnd/Project_1/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { LoginComponent } from './login/login.component';
 import { Pagina2Component } from './pagina2/pagina2.component';
 import { AuthGuard } from './auth.guard';
 import { importProvidersFrom } from '@angular/core';
+import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
-    { path: 'Principal', component: Pagina2Component ,canActivate:[AuthGuard] },
+    { path: 'Principal', component: Pagina2Component ,canActivate:[AuthGuard] }, { path: 'ForgotPassword', component: RecuperarPasswordComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' }
   ];
