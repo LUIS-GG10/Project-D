d15721b8e8e1fb611bed56ce90fb753188c77363
LUIS-GG10 <luis_enggg@hotmail.com>
2024-08-21 11:12:48 -0600

    Server Crud Done

--- BackEnd/Conexion/src/models/entitites/Server.py
+++ BackEnd/Conexion/src/models/entitites/Server.py
@@ -1,13 +1,27 @@
 class Server():
     #Iniciacion de objeto user
-    def __init__(self,id,name=None,password=None):
+    def __init__(self,id,name=None,password=None,hashPassword=None,HostName=None,OS=None,IP=None,PhysicalServer=None,LogLocation=None,Type=None):
         self.id=id
         self.name=name
         self.password=password
+        self.hashPassword=hashPassword
+        self.HostName=HostName
+        self.OS=OS
+        self.IP=IP
+        self.PhysicalServer=PhysicalServer
+        self.LogLocation=LogLocation
+        self.Type=Type
     #Metodo de respuesta Json
     def to_JSON(self):
         return {
             'id':self.id,
             'name':self.name,
             'password':self.password,   
+            'hashPassword':self.hashPassword,
+            'HostName':self.HostName,
+            "OS":self.OS,
+            "IP":self.IP,
+            "PhysicalServer":self.PhysicalServer,
+            "LogLocation":self.LogLocation,
+            "Type":self.Type
         }
