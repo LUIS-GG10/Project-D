704c548193d6a9d1db6b6aeac3d5e7d12255603d
LUIS-GG10 <luis_enggg@hotmail.com>
2024-08-26 14:24:35 -0600

    Delete Method

--- FrontEnd/Project_1/src/app/auth.service.ts
+++ FrontEnd/Project_1/src/app/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private authenticated = true;
+  private authenticated = false;
 
   login(validate: boolean): boolean {
     if (validate) { 
--- FrontEnd/Project_1/src/app/pagina2/componentesDU/borrar/borrar.component.ts
+++ FrontEnd/Project_1/src/app/pagina2/componentesDU/borrar/borrar.component.ts
@@ -5,17 +5,43 @@ import { MatFormField } from "@angular/material/form-field";
 import { MatLabel } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import { HttpClient} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-modificar',
   standalone: true,
-  imports: [MatCardContent, MatCardTitle, MatCard, MatCardActions, MatCardHeader, MatDivider,MatFormField, MatLabel, MatInputModule],
+  imports: [HttpClientModule,MatCardContent, MatCardTitle, MatCard, MatCardActions, MatCardHeader, MatDivider,MatFormField, MatLabel, MatInputModule],
   templateUrl: './borrar.component.html',
   styleUrl: './borrar.component.css'
 })
 export class borrarComponent {
+  id:string|null=''
 onAccion() {
-throw new Error('Method not implemented.');
-}
+this.sendData();
 
+
+}
+constructor(private http: HttpClient) {
+    
+}
+sendData() {
+  // Encode the credentials (if necessary)
+  const id = 2;
+  const data={
+    id:id
+  }
+
+
+  // Construct the request URL with the parameters
+  const url = `http://127.0.0.1:5000/api/Principal/`+id;
+
+  // Make the GET request
+  this.http.delete(url).subscribe(response => {
+    console.log('Success:', response);
+    
+  }, error => {
+    console.error('Error:', error);
+  });
+}
 }
